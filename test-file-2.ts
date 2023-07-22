99a7ed923a82f28a02ada197c93a4c73ecdcb53a
Tharak-N <tharak.n@innovatily.com>
2023-07-22 14:59:59 +0530

    minor changes

---
+++ test-file-2.ts
@@ -0,0 +1,9 @@
+
+
+// import { copy } from 'https://deno.land/std@0.195.0/fs/copy.ts'
+// import { red, gray, bold } from 'https://deno.land/std@0.195.0/fmt/colors.ts'
+// console.log(gray('tharak'), bold('Be Bold'))
+
+
+import { red } from 'std/fmt/colors.ts';
+console.log(red("hello tharak"))
